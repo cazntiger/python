3c9fa583b5f64c6b3af22aab65819da211df3230
Jason Lee <fordlee.work@gmail.com>
2025-03-15 22:19:52 -0400

    updated calculations

--- tax_calc.py
+++ tax_calc.py
@@ -26,40 +26,66 @@ program_run = True
 while program_run:
     print("Welcome to the income calculator.  Please provide an income to calculate the tax.")
 
-    income_str = input("Your income: ")
-    income = int(income_str)
+    w2_income_str = input("Your w2 income: ")
+    w2_income = int(w2_income_str)
 
-    if income <= TAXL1:
-        calc_tax = calculateTax(income, TAXL1R)
-    elif income <= TAXL2:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(income-TAXL1, TAXL2R)
-    elif income <= TAXL3:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(income-TAXL2, TAXL3R)
-    elif income <= TAXL4:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(income-TAXL3, TAXL4R)
-    elif income <= TAXL5:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(income-TAXL4, TAXL5R)
-    elif income <= TAXL6:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(TAXL5-TAXL4, TAXL5R) + calculateTax(income-TAXL5, TAXL6R)
+    interests_str = input("You taxable interests earnings: ")
+    interests = int(interests_str)
+
+    dividends_str = input("Your stock dividends: ")
+    dividends = int(dividends_str)
+
+    capital_gains_str = input("Your capital gains: ")
+    capital_gains = int(capital_gains_str)
+
+    other_income_str = input("Other income: ")
+    other_income = int(other_income_str)
+
+    retirement_contributions_str = input("Retirement contributions: ")
+    retirement_contributions = int(retirement_contributions_str)
+
+    standard_deduction_str = input("Standard deduction: ")
+    standard_deduction = int(standard_deduction_str)
+    
+    total_income = w2_income + interests + dividends + capital_gains + other_income
+
+    adjusted_gross_income = total_income - retirement_contributions
+
+    taxable_income = adjusted_gross_income - standard_deduction
+
+
+    w2_stateTax = calculateTax(w2_income, STATER)
+    w2_taxWithheld = 0.12*w2_income
+
+    if taxable_income <= TAXL1:
+        calc_tax = calculateTax(taxable_income, TAXL1R)
+    elif taxable_income <= TAXL2:
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(taxable_income-TAXL1, TAXL2R)
+    elif taxable_income <= TAXL3:
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(taxable_income-TAXL2, TAXL3R)
+    elif taxable_income <= TAXL4:
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(taxable_income-TAXL3, TAXL4R)
+    elif taxable_income <= TAXL5:
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(taxable_income-TAXL4, TAXL5R)
+    elif taxable_income <= TAXL6:
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(TAXL5-TAXL4, TAXL5R) + calculateTax(taxable_income-TAXL5, TAXL6R)
     else:
-        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(TAXL5-TAXL4, TAXL5R) + calculateTax(TAXL6-TAXL5, TAXL6R) + calculateTax(income-TAXL6, TAXL7R)
+        calc_tax = calculateTax(TAXL1, TAXL1R) + calculateTax(TAXL2-TAXL1, TAXL2R) + calculateTax(TAXL3-TAXL2, TAXL3R) + calculateTax(TAXL4-TAXL3, TAXL4R) + calculateTax(TAXL5-TAXL4, TAXL5R) + calculateTax(TAXL6-TAXL5, TAXL6R) + calculateTax(taxable_income-TAXL6, TAXL7R)
 
-    stateTax = calculateTax(income, STATER)
-    # stateTax = 2000
-    taxWithheld = 0.12*income
+    stateTax = 0.06*taxable_income
 
     print("The total federal tax is: ", calc_tax)
     print("The total state tax is: ", stateTax)
     print("The total tax is: ", calc_tax+stateTax)
-    print("The income before federal tax is: ", income)
-    print("The income after federal tax is: ", income-calc_tax)
-    print("The income after federal and state tax is: ", income-calc_tax-stateTax)
-    print("The withholding tax is: ", taxWithheld)
+    print("The income before federal tax is: ", taxable_income)
+    print("The income after federal tax is: ", taxable_income-calc_tax)
+    print("The income after federal and state tax is: ", taxable_income-calc_tax-stateTax)
+    print("The withholding tax is: ", w2_taxWithheld)
 
-    if taxWithheld > calc_tax+stateTax:
-        print("You will get a refund of: ", taxWithheld-calc_tax-stateTax)
+    if w2_taxWithheld > calc_tax+stateTax:
+        print("You will get a refund of: ", w2_taxWithheld-calc_tax-stateTax)
     else:
-        print("You owe: ", calc_tax+stateTax-taxWithheld)
+        print("You owe: ", calc_tax+stateTax-w2_taxWithheld)
 
     program_run = input("Do you want to run the program again? (yes/no): ")
     if program_run == "no":
